66e00847ab1e1d0efeaf5adc7fd053e8ad6998e5
Ignacio DM <i.dalmaumiravitlles@gmail.com>
2025-04-28 12:26:33 +0200

    Guardar ades

--- src/prog2/adaptador/Adaptador.java
+++ src/prog2/adaptador/Adaptador.java
@@ -23,13 +23,11 @@ import java.util.List;
 //detalls d’implementació del model.
 public class Adaptador {
     private float demandaPotencia; // Atribut que manté la demanda de potència
-    public Bitacola bitacola;
     public Dades dades;
 
     // Constructor per inicialitzar la demanda de potència
     public Adaptador() {
         this.demandaPotencia = demandaPotencia;
-        this.bitacola = new Bitacola();
         this.dades = new Dades();
     }
 
@@ -102,7 +100,7 @@ public class Adaptador {
 
     public void guardaDades(String camiDesti) throws CentralUBException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(camiDesti))) {
-            writer.write(bitacola.toString());
+            writer.write(dades.mostraBitacola().toString());
         } catch (Exception e) {
             throw new CentralUBException("Error guardant les dades: " + e.getMessage());
         }
