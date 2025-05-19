8194b29ebd6655af3bab913290bcc12cbb534598
Ignacio DM <i.dalmaumiravitlles@gmail.com>
2025-05-19 11:48:23 +0200

    fixed ativar i desactivar bombes

--- src/prog2/vista/Components_Central.java
+++ src/prog2/vista/Components_Central.java
@@ -5,12 +5,15 @@ import prog2.model.Reactor;
 import prog2.model.SistemaRefrigeracio;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class Components_Central extends JDialog {
     CentralUB centralUB;
     float insercioBarres;
     Reactor reactor;
     SistemaRefrigeracio refrigeracio;
+    private JSpinner spinnerBarres; //Fusionar events
     private JPanel contentPane;
     private JSlider sliderBarres;
     private JButton buttonactivarReactor;
@@ -28,5 +31,30 @@ public class Components_Central extends JDialog {
         setContentPane(contentPane);
         setSize(600, 500);
         setModal(true);
+
+
+        // Configurar Slider (0-100%)
+        sliderBarres.setMinimum(0);
+        sliderBarres.setMaximum(100);
+        sliderBarres.setValue((int)(centralUB.getAdaptador().getInsercioBarres() * 100));
+
+        // Configurar Spinner (mateix rang que el slider)
+        SpinnerNumberModel spinnerModel = new SpinnerNumberModel(sliderBarres.getValue(), 0, 100, 1);
+        spinnerBarres.setModel(spinnerModel);
+
+        // Vincular canvis del spinner amb el slider
+        spinnerBarres.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int valor = (int) spinnerBarres.getValue();
+                sliderBarres.setValue(valor); // Actualitza slider
+                try {
+                    centralUB.getAdaptador().setInsercioBarres(valor / 100.0f); // Actualitza model
+                } catch (CentralUBException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
     }
+
 }
