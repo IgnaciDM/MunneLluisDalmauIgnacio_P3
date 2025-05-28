bd7356101deb6f2311ea46b4eefb18f9af29265c
Ignacio DM <i.dalmaumiravitlles@gmail.com>
2025-05-28 23:21:25 +0200

    Practica Examen: EstatTecnic

--- src/prog2/model/EstatTecnic.java
+++ src/prog2/model/EstatTecnic.java
@@ -1,29 +1,40 @@
 package prog2.model;
 
-import java.awt.datatransfer.FlavorListener;
+import prog2.vista.CentralUBException;
 
 public class EstatTecnic {
     private Reactor reactor;
     private BombaRefrigerant bombaRefrigerant;
     private SistemaRefrigeracio sistemaRefrigeracio;
+    float insercioBarres;
+    private Dades dades;
 
-
-    EstatTecnic(Reactor reactor) {
+    public EstatTecnic(Reactor reactor,BombaRefrigerant bombaRefrigerant,SistemaRefrigeracio sistemaRefrigeracio) {
         this.reactor = reactor;
         this.bombaRefrigerant= bombaRefrigerant;
+        this.sistemaRefrigeracio = sistemaRefrigeracio;
+        this.insercioBarres = dades.getInsercioBarres();
     }
-    void RepararReactor(){
 
+    void RepararReactor(){
+        for (float i=insercioBarres; i>0 ;i++) {
+            try {
+                dades.setInsercioBarres(100);
+            } catch (CentralUBException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     void RepararBombes(int num){
         for (BombaRefrigerant bomba: sistemaRefrigeracio.getllistabombes()) {
-
+            if (bomba.getId()==num){
+                bombaRefrigerant.setForaDeServei(false);
+            }
         }
     }
 
-    float CalcularCost(){
-
+    float CalcularCostosReparacio(){
         return 0;
     }
 
